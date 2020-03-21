5d83392799a5bc083ddc7380e62ed477e7802ede
Smith <smith.mugalu@andela.com>
2020-03-21 17:07:08 +0300

    added new xcode files to project

---
+++ Croesus/Croesus/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Croesus
+//
+//  Created by Smithers on 21/03/2020.
+//  Copyright © 2020 Smithers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
