867f27625c5fd45d108764d69c15a62946c2f36b
Smith <smith.mugalu@andela.com>
2020-03-21 18:53:42 +0300

    finished setting up firebase

---
+++ Croesus/Pods/Target Support Files/Pods-Croesus-CroesusUITests/Pods-Croesus-CroesusUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Croesus_CroesusUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Croesus_CroesusUITestsVersionString[];
+
